7f37ae52e6d6f2966e03d0b60de318e8f6e66f29
Daniil Neretin <dneretin01@gmail.com>
2026-08-25 12:02:43 +0300

    del old

--- mail_.php
+++
@@ -1,6 +0,0 @@
-<?
-if (mail("tsit@ucp.by","test subject", "test body","From: otpravitel@bitrix.ru"))
-echo "Сообщение передано функции mail, проверьте почту в ящике.";
-else
-echo "Функция mail не работает, свяжитесь с администрацией хостинга.";
-?>
