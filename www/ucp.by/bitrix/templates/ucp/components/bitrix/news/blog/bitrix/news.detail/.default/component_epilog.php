2667cddb86c6ec1a892318d23482c9705cba0385
Daniil Neretin <dneretin01@gmail.com>
2026-08-29 17:23:07 +0300

    update project

--- www/ucp.by/bitrix/templates/ucp/components/bitrix/news/blog/bitrix/news.detail/.default/component_epilog.php
+++
@@ -1,6 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-global $APPLICATION;
-if (isset($arResult['NAME']))
-	$APPLICATION->SetTitle($arResult["NAME"]);
-?>
--- www/ucp.by/bitrix/templates/ucp/components/bitrix/news/blog/bitrix/news.list/.default/component_epilog.php
+++
@@ -1,8 +0,0 @@
-<?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-if (is_array($arResult["SECTION"]["PATH"]))
-{
-    $s = array_pop($arResult["SECTION"]["PATH"]);
-    $GLOBALS['APPLICATION']->SetTitle($s["NAME"]);
-}
-?>
