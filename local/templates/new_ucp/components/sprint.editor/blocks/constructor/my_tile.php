9aec39d83154c6570a0200c1ddc2f951c6ba8bdf
Daniil Neretin <dneretin01@gmail.com>
2026-09-10 16:25:55 +0300

    fix tiles

--- local/templates/new_ucp/components/sprint.editor/blocks/constructor/my_tile.php
+++ local/templates/new_ucp/components/sprint.editor/blocks/constructor/my_tile.php
@@ -5,11 +5,22 @@
 <?php
 $item = $block['items'][0];
 ?>
-<li id="bx_3099439860_29135" class="home__feed-nav-item">
-    <a href="<?=$item['url']?>"
-       style="background: url(<?=$item['image']['ORIGIN_SRC']?>) #d3d3d3 50%/cover no-repeat;">
-        <img loading="lazy" src="<?=$item['icon']['ORIGIN_SRC']?>"
-             alt="<?=$item['title']?>">
-        <p><?=$item['title']?></p>
+<li class="home__feed-nav-item">
+    <a href="<?=htmlspecialcharsbx($item['url'])?>"
+            <?php if (!empty($item['image']['ORIGIN_SRC'])): ?>
+                style="background: url('<?=htmlspecialcharsbx($item['image']['ORIGIN_SRC'])?>') #d3d3d3 50%/cover no-repeat;"
+            <?php else: ?>
+                style="background: #d3d3d3;"
+            <?php endif; ?>
+    >
+        <?php if (!empty($item['icon']['ORIGIN_SRC'])): ?>
+            <img
+                    loading="lazy"
+                    src="<?=htmlspecialcharsbx($item['icon']['ORIGIN_SRC'])?>"
+                    alt="<?=htmlspecialcharsbx($item['title'])?>"
+            >
+        <?php endif; ?>
+
+        <p><?=htmlspecialcharsbx($item['title'])?></p>
     </a>
 </li>
--- local/templates/new_ucp/components/sprint.editor/blocks/constructor/my_tiles.php
+++ local/templates/new_ucp/components/sprint.editor/blocks/constructor/my_tiles.php
@@ -2,10 +2,47 @@
  * @var $block array
  * @var $this  SprintEditorBlocksComponent
  */ ?>
-<nav class="home__feed-nav">
-    <ul class="home__feed-nav-list">
-        <?php foreach ($block['blocks'] as $itemblock) { ?>
-            <?php $this->includeBlock($itemblock) ?>
-        <?php } ?>
-    </ul>
-</nav>
+<?php if (!empty($block['blocks'])): ?>
+
+    <?php
+    $total = count($block['blocks']);
+    $rows = [];
+
+    $rowCount = (int)ceil($total / 5);
+
+    while ($rowCount > 1 && ceil($total / $rowCount) < 3) {
+        $rowCount--;
+    }
+
+    $base = intdiv($total, $rowCount);
+    $remainder = $total % $rowCount;
+
+    for ($i = 0; $i < $rowCount; $i++) {
+        $rows[] = $base + ($i < $remainder ? 1 : 0);
+    }
+
+    $blockIndex = 0;
+    ?>
+
+    <nav class="home__feed-nav" style="height: 150px;">
+
+        <?php foreach ($rows as $rowCount): ?>
+
+            <ul
+                    class="home__feed-nav-list"
+                    style="height:100%; grid-template-columns: repeat(<?= $rowCount ?>, 1fr);"
+            >
+                <?php for ($i = 0; $i < $rowCount; $i++): ?>
+
+                    <?php $this->includeBlock($block['blocks'][$blockIndex]); ?>
+
+                    <?php $blockIndex++; ?>
+
+                <?php endfor; ?>
+            </ul>
+
+        <?php endforeach; ?>
+
+    </nav>
+
+<?php endif; ?>
